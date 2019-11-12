1bf2c1a2d6c881b6379fe5c479aebf1a0605a56c
drsdrs456 <drs1070702@126.com>
2019-11-12 21:38:24 +0800

    提交leetcode26题

---
+++ leetcode26.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {       
+        int i = nums.length > 0 ? 1:0;
+        for(int n : nums){
+            if( n != nums[i-1]){                
+                nums[i] = n;
+                i++;
+            }
+        }
+        return i;
+    }
+}
